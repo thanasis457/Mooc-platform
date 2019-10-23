5eab88ab06cbe09410f3ad609b4af5acae38f267
Athanasios Taprantzis <thatapras@hotmail.gr>
2019-10-24 00:46:22 +0300

    More improvements and better UI

--- scraper/futurelearn.py
+++ scraper/futurelearn.py
@@ -20,11 +20,12 @@ class FutureLearn(Platform):
         return urls
 
     def _parse(self, url):
+        courses=[]
         res = requests.get(url)
         soup = bs4.BeautifulSoup(res.text, 'lxml')
 
         title = soup.select('h1')[0].text.strip()
-        
+
         description = '\n'.join([p.text for p in soup.select('#section-overview p')])
         snippet = soup.select('h1 + p')[0].text
 
@@ -32,14 +33,15 @@ class FutureLearn(Platform):
         org_images = soup.select('.m-dual-billboard .a-standard-org-logo a img')
         for image in org_images:
             partners.append(image.get('title'))
-        
+
         tags = []
         category_links = soup.select('.run-related p.old-text-typescale a')
         for link in category_links:
             tag = link.text.replace('\xa0', ' ')
             tags.append(tag)
 
-        course = Course(title, partners, self.name, 
+        course = Course(title, partners, self.name,
                         description, tags, url, snippet=snippet)
-        return [course]
+        courses.append(course)
 
+        return courses
