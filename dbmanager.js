26a36f66970612746ecbea17a715e86ea63aacb8
Athanasios Taprantzis <thatapras@hotmail.gr>
2020-01-31 21:18:58 +0200

    Minor change

--- dbmanager.js
+++ dbmanager.js
@@ -30,22 +30,22 @@ function insert_into_db(object){
     //   });
     //   // console.log(i);
     // }
-    // con.query("SELECT * FROM courses ORDER BY hits DESC LIMIT 5",function(err,result){
-    //   if(err) throw err;
-    //   console.log(result);
-    //   con.end();
-    // });
+    con.query("SELECT UPenn FROM courses ORDER BY hits DESC LIMIT 5",function(err,result){
+      if(err) throw err;
+      console.log(result);
+      con.end();
+    });
     // con.query("SELECT * FROM courses WHERE name='The Value of Business Models'",function(err,result){
     //   if(err) throw(err);
     //   console.log(result);
     //   con.end();
     // });
-    var sql='ALTER TABLE courses ADD (Caltech INT DEFAULT 0,MIT INT DEFAULT 0, Stanford INT DEFAULT 0, Princeton INT DEFAULT 0, Columbia INT DEFAULT 0, Harvard INT DEFAULT 0, Georgia INT DEFAULT 0, UC INT DEFAULT 0, Yale INT DEFAULT 0, UCLA INT DEFAULT 0, Northwestern INT DEFAULT 0, Boston INT DEFAULT 0, Michigan INT DEFAULT 0, UChicago INT DEFAULT 0, Brown INT DEFAULT 0, Toronto INT DEFAULT 0)';
-    con.query(sql,function(err,result){
-      if(err) throw err;
-      console.log(result);
-      con.end();
-    });
+    // var sql='ALTER TABLE courses ADD (UPenn INT DEFAULT 0)';
+    // con.query(sql,function(err,result){
+    //   if(err) throw err;
+    //   console.log(result);
+    //   con.end();
+    // });
   });
 }
 insert_into_db("Anything");
